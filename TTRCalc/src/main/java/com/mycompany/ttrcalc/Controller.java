d293cb99fe7d64d6102e6d2de2f2bc40485bdf80
David Charles <charled1@gmatc.matc.edu>
2022-05-20 15:59:20 -0500

    Games and Scoring loops in place.

--- TTRCalc/src/main/java/com/mycompany/ttrcalc/Controller.java
+++ TTRCalc/src/main/java/com/mycompany/ttrcalc/Controller.java
@@ -74,10 +74,11 @@ public class Controller {
         boolean cont = true;
         String action = "";
         String[] pNames;
-        String quickHelp = "Enter a command (List, a player's name, Help):";
-        String controlls = "When a player scores or draws a new ticket you will need to enter that information into the app."
-                + "In order to do that you will need to enter information for that player.  Enter the player's name in order to view"
-                + "thier current score stats as well as record a new score!  (Type HELP to see this message again)";
+        String quickHelp = "Enter a command (List, A Player's name, All, Help):";
+        String controlls = "When a player scores or draws a new ticket you will need to enter that information into the app.\n"
+                + "In order to do that you will need to enter the player's name in order to view"
+                + "thier current score stats as well as record a new score!\n"
+                + "(Type HELP to see this message again)";
         
         
         v.printString(controlls);
@@ -89,6 +90,7 @@ public class Controller {
         
         do{
         //main loop
+        v.printString(v.a.mMenu + "\n");
         v.printString(quickHelp);
         action = v.inString();
         
@@ -99,9 +101,15 @@ public class Controller {
                     list = list + allPlayers[i].getpName() + "\n";
                 }
                 v.printString(list);
+                v.enterToCont();
                 break;
             case "help":
                 v.printString(controlls);
+                v.enterToCont();
+                break;
+            case "all":
+                allPlayers();
+                v.enterToCont();
                 break;
             default:
                 if(isPlayer(action)){
@@ -136,8 +144,29 @@ public class Controller {
         
     }
 
-    private void playerAction(Player player) {
-        v.printString("Selected player: " + player.getpName());
+    private void playerAction(Player player) throws InterruptedException {
+        String pInstructions = "Enter a command: Score, Back";
+        String action = "";
+        
+        
+        v.printString(v.a.pStats);
+        v.printString(player.toString());
+        v.waitASec();
+        v.printString(pInstructions);
+        action = v.inString();
+        
+        switch(action){
+            case "back":
+                break;
+            case "score":
+               // v.printString("Scoring");
+                playerScore(player);
+                break;
+            default:
+                v.printString("Unknown value, please try again");
+                v.enterToCont();
+                break;
+        }
     }
         
         
@@ -148,4 +177,60 @@ public class Controller {
 //            v.printString(allPlayers[i].toString());
 //        }
 //        v.printString(Integer.toString(gameType));
+
+    private void allPlayers() {
+        for(int i=0;i<allPlayers.length;i++){
+            v.printString(allPlayers[i].toString());
+        }
+    }
+
+    private void playerScore(Player player) throws InterruptedException {
+        int trains;
+        int points=0;
+        boolean cont;
+        
+        v.printString(v.a.score);
+        v.waitASec();
+
+        
+        do{
+            v.printString("How many Trains did " + player.getpName() + " score?");
+            v.printString("**(Enter 0 to cancel)**");
+            cont = false;
+            trains = v.inInt();
+            switch(trains){
+                case 1:
+                    points =1;
+                    break;
+                case 2:
+                    points =2;
+                    break;
+                case 3:
+                    points =4;
+                    break;
+                case 4:
+                    points =7;
+                    break;
+                case 5:
+                    points =10;
+                    break;
+                case 6:
+                    points =15;
+                    break;
+                case 0:
+                    break;
+                default:
+                    v.printString(Integer.toString(trains) + " is not a valid number, please try again");
+                    cont = true;
+                    
+                    v.enterToCont();                    
+            }
+        }while(cont == true);
+        
+        player.setpScore(player.getpScore() + points);
+        v.printString("A route with " +Integer.toString(trains) + " trains is worth " + 
+                Integer.toString(points) + " points.");
+        v.printString(player.getpName() + "'s score is now: " + player.getpScore());
+        v.enterToCont();
+    }
 }
